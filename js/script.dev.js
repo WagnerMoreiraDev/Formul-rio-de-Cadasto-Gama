a4ddd411a7693c39a23cdd3bb3c5578ad8284f56
VISAOTECH <84467663+VISAOTECH@users.noreply.github.com>
2021-08-27 19:30:54 -0300

    atualização nos arquivos

--- js/script.dev.js
+++ js/script.dev.js
@@ -1,68 +1,49 @@
-// EStá é uma diretiva de ECMAScript 5
-"use strict";
-
 // Uma funcção
 function carregaEndereco() 
 {
   // O método "getElementById" checa a caixa de entrada pelo identificador "cep"
-  cep = document.getElementById("cep").value;
+  cep = document.getElementById('cep').value;
 
   //  Verifica se não é igual a 8 números
-  if (cep.length != 8) 
-  {
-    // Alarma que deve escrever 8Números
-    alert("CEP precisa ter 8 números");
-    return;
-  }
+    if (cep.length != 8) 
+    {
+      // Alarma que deve escrever 8 números
+      alert("CEP precisa ter 8 números");
+      return;
+    }
+  
 
   // Pega o endereço, concatenando o subdomínio JSON
-  url = "https://viacep.com.br/ws/".concat(cep, "/json/");
+  url = `https://viacep.com.br/ws/${cep}/json/`;
   // Checa e baixa o arquivo JSON
-  fetch(url).then(function _callee(res) 
-  {
-    // Assincrona e regenera o arquivo JSON
-    return regeneratorRuntime.async(function _callee$(_context) 
+  fetch(url)
+    .then(async (res) => 
     {
-      // Repete a sincronização do arquivo baixado
-      while (1) 
+      // Assincrona e regenera o arquivo JSON
+      endereco = await res.json()
+
+      // Checa se o CEP está errado
+      if (endereco.erro) 
       {
-        // Troca de casos para checar se o CEP é válido ou inválido;
-        switch (_context.prev = _context.next) 
-        {
-          case 0:
-            _context.next = 2;
-            return regeneratorRuntime.awrap(res.json());
-
-          case 2:
-            endereco = _context.sent;
-
-            if (!endereco.erro) 
-            {
-              _context.next = 7;
-              break;
-            }
-
-            alert("Número de CEP inválido");
-            document.getElementById("cep").focus();
-            return _context.abrupt("return");
-
-          case 7:
-            document.getElementById("endereco").value = endereco.logradouro;
-            document.getElementById("bairro").value = endereco.bairro;
-            document.getElementById("cidade").value = endereco.localidade;
-            document.getElementById("estado").value = endereco.uf;
-
-          case 11:
-          case "end":
-            return _context.stop();
-        }
+        // Avisa que o está errado
+        alert("Número de CEP inválido");
+        // O método "getElementById" checa a caixa de entrada pelo identificador "cep"
+        document.getElementById("cep").focus();
+        return;
       }
-    }); // Caso dê errado
-  })["catch"](function () 
-  {
-    alert("Algo deu errado com seu CEP");
-    document.getElementById("cep").focus();
-  });
+
+      // O método "getElementById" checa as caixas de entradas pelos identificadores para verificar se estão preenchidos
+      document.getElementById("endereco").value = endereco.logradouro;
+      document.getElementById("bairro").value = endereco.bairro;
+      document.getElementById("cidade").value = endereco.localidade;
+      document.getElementById("estado").value = endereco.uf;
+    }) // Caso dê errado
+    .catch(() => 
+    {
+      // A insistência do mesmo aviso que ainda há errado com o CEP
+      alert("Algo deu errado com seu CEP");
+      document.getElementById("cep").focus();
+    });
 }
 
 // Caso todos os campos sejam preenchidos, é aceito enviar
